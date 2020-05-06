33b1d91b640f115bd46fd32fc3d910d07a42484e
DiegoDc33 <diegodechiara@outlook.it>
2020-05-06 15:57:49 +0200

    aggiunta schermata login e registrazione

---
+++ app/src/main/java/com/example/myapplication/login_fragment.kt
@@ -0,0 +1,37 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_login.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class login_fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnCreate.setOnClickListener {
+            Navigation.findNavController(it)
+                .navigate(R.id.action_login_fragment_to_register_fragment)
+
+
+        }
+
+    }
+}
+
+
+
