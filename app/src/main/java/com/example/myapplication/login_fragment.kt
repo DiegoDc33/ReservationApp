027231d78540e16d1e6ee7b3fbfd89fb950a81fc
DiegoDc33 <diegodechiara@outlook.it>
2020-05-08 11:35:47 +0200

    implementazione login e registrazione (con errore) errore in auth.createUserWithEmailAndPassword in register_fragment. addOnCompleteListener e unresolved reference "isSuccessful" e "baseContext".

--- app/src/main/java/com/example/myapplication/login_fragment.kt
+++ app/src/main/java/com/example/myapplication/login_fragment.kt
@@ -1,17 +1,22 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.fragment_login.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class login_fragment : Fragment() {
+    private lateinit var auth: FirebaseAuth
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +25,29 @@ class login_fragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+
+
+
+
+
+    }
+
+
+
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    fun updateUI(currentUser:FirebaseUser?) {}
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/com/example/myapplication/register_fragment.kt
+++ app/src/main/java/com/example/myapplication/register_fragment.kt
@@ -1,16 +1,30 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragment_register.*
+import kotlinx.android.synthetic.main.fragment_login.btnCreate as btnCreate1
+import kotlinx.android.synthetic.main.fragment_register.*
+import kotlinx.android.synthetic.main.fragment_login.editEmail as editEmail1
+import kotlinx.android.synthetic.main.fragment_register.editPassword as editPassword1
+import com.google.android.gms.tasks.OnSuccessListener
+import android.content.Intent
+import com.google.firebase.auth.AuthResult
+import android.util.Log
+import android.widget.Toast
 
 /**
  * A simple [Fragment] subclass.
  */
 class register_fragment : Fragment() {
-
+    private lateinit var auth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,4 +33,75 @@ class register_fragment : Fragment() {
         return inflater.inflate(R.layout.fragment_register, container, false)
     }
 
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+        btnCreate.setOnClickListener {
+            signUpUser()
+        }
+
+    }
+
+private fun signUpUser() {
+
+    if (editEmail.text.toString().isEmpty()) {
+        editEmail.error = "Per favore inserisci email"
+        editEmail.requestFocus()
+        return
+    }
+    if (!Patterns.EMAIL_ADDRESS.matcher(editEmail.text.toString()).matches()) {
+        editEmail.error = "Per favore inserisci email valida"
+        editEmail.requestFocus()
+        return
+    }
+    if (editPassword.text.toString().isEmpty()) {
+        editPassword.error = "Per favore inserisci password"
+        editPassword.requestFocus()
+        return
+    }
+
+
+    auth.createUserWithEmailAndPassword(editEmail.text.toString(), editPassword.text.toString())
+        .addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                Navigation.findNavController(it)
+                    .navigate(R.id.action_register_fragment_to_login_fragment)
+
+
+            } else {
+
+                Toast.makeText(
+                    baseContext, "Authentication failed.",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
+
+        }
+
+
 }
+
+
+
+
+
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnFreccia.setOnClickListener {
+            Navigation.findNavController(it)
+                .navigate(R.id.action_register_fragment_to_login_fragment)
+
+
+        }
+
+    }
+
+}
+
+
