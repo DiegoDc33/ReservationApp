9113c4ed2c4af9b2f9fc263cc769bfac44f41a6f
DiegoDc33 <diegodechiara@outlook.it>
2020-05-09 17:24:20 +0200

    implementazione login con errore ho tolto reindirizzamento al frament menu perchè all'avvio bypassava schermata login login fallito anche con dati corretti

--- app/src/main/java/com/example/myapplication/login_fragment.kt
+++ app/src/main/java/com/example/myapplication/login_fragment.kt
@@ -2,14 +2,20 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragment_login.btnCreate
+import kotlinx.android.synthetic.main.fragment_register.*
+import kotlinx.android.synthetic.main.fragment_login.editEmail as editEmail1
+import kotlinx.android.synthetic.main.fragment_login.editPassword as editPassword1
 
 /**
  * A simple [Fragment] subclass.
@@ -27,13 +33,49 @@ class login_fragment : Fragment() {
     }
 
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         auth = FirebaseAuth.getInstance()
-
-
-
     }
+
+
+
+    private fun doLogin() {
+
+        if (editEmail.text.toString().isEmpty()) {
+            editEmail.error = "Per favore inserisci email"
+            editEmail.requestFocus()
+            return
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(editEmail.text.toString()).matches()) {
+            editEmail.error = "Per favore inserisci email valida"
+            editEmail.requestFocus()
+            return
+        }
+        if (editPassword.text.toString().isEmpty()) {
+            editPassword.error = "Per favore inserisci password"
+            editPassword.requestFocus()
+            return
+        }
+
+        auth.signInWithEmailAndPassword(editEmail.toString(), editPassword.toString())
+            .addOnCompleteListener {
+
+                if (it.isSuccessful) {
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+
+                    updateUI(null)
+                }
+
+            }
+    }
+
+
+
 
 
     public override fun onStart() {
@@ -43,15 +85,33 @@ class login_fragment : Fragment() {
         updateUI(currentUser)
     }
 
-    fun updateUI(currentUser:FirebaseUser?) {}
+    private fun updateUI(currentUser:FirebaseUser?) {
+        if(currentUser !=null){
+            Toast.makeText(requireContext(), "PROVA SENZA REINDIRIZZAMENTO",
+                Toast.LENGTH_SHORT).show()
+           /* Navigation.findNavController(requireActivity(), R.id.NavHost)
+
+                .navigate(R.id.action_login_fragment_to_menu_fragment)*/
+        }else{
+            Toast.makeText(requireContext(), "Authentication failed.",
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+
+
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         btnCreate.setOnClickListener {
             Navigation.findNavController(it)
                 .navigate(R.id.action_login_fragment_to_register_fragment)
-
-
+        }
+        btnLogin.setOnClickListener {
+            doLogin()
         }
 
     }
